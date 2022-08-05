7a9c81c2d049b1fca2be1d97a12ec9a6a13644a2
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-08-04 21:46:16 -0300

    Login no heroku

--- class/config.php
+++ class/config.php
@@ -1,10 +1,15 @@
 <?php
   //Configuração do banco de dados
 
-  define('SERVIDOR','localhost');
-  define('USUARIO','root');
-  define('SENHA','');
-  define('BANCO','cliente');
+  // define('SERVIDOR','localhost');
+  // define('USUARIO','root');
+  // define('SENHA','');
+  // define('BANCO','cliente');
+
+  define('SERVIDOR','us-cdbr-east-06.cleardb.net');
+  define('USUARIO','bdb07e006ad878');
+  define('SENHA','0550b188');
+  define('BANCO','heroku_bf681ee132a9fbb');
   
   //Retirar espaço, caracteres especiais etc
   function limpaPost($dados){
