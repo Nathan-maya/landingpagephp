e7e85db9c747c2c5e50e41966047dbca77283946
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-08-05 14:16:18 -0300

    trocando banco de dados

--- class/config.php
+++ class/config.php
@@ -10,11 +10,19 @@
     define('BANCO','cliente');  
   }
 
+  // if($modo == 'producao'){
+  //   define('SERVIDOR','us-cdbr-east-06.cleardb.net');
+  //   define('USUARIO','bdb07e006ad878');
+  //   define('SENHA','0550b188');
+  //   define('BANCO','heroku_bf681ee132a9fbb');
+  // }
+
+  
   if($modo == 'producao'){
-    define('SERVIDOR','us-cdbr-east-06.cleardb.net');
-    define('USUARIO','bdb07e006ad878');
-    define('SENHA','0550b188');
-    define('BANCO','heroku_bf681ee132a9fbb');
+    define('SERVIDOR','cxmgkzhk95kfgbq4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+    define('USUARIO','ot1q1lq6t7drvnot');
+    define('SENHA','x122sooh9983z6dy');
+    define('BANCO','iz0kffffflhvreu9');
   }
 
   
