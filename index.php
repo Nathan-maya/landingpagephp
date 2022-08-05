162b9759513ef53d8f211e32d28c3c3ce35f7e8d
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-08-04 23:00:21 -0300

    ajustes php mailer

--- index.php
+++ index.php
@@ -5,9 +5,8 @@ require_once('autoload.php');
 // REQUERIMENTO DO PHPMAILER
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-
-require 'config/PHPMailer/src/Exception.php';
-require 'config/PHPMailer/src/SMTP.php';
+require 'class/config/PHPMailer/src/Exception.php';
+require 'class/config/PHPMailer/src/SMTP.php';
 
 if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['mensagem'])) {
 
