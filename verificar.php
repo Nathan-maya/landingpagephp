f8f1be5db02e735ed289cf869ea8300e42cec284
nathan.maia <Nathan_maia@outlook.com>
2022-08-06 17:39:04 -0300

    ajustes erro recaptcha

--- verificar.php
+++ verificar.php
@@ -1,15 +1,13 @@
-<?php 
+<?php
 $captcha = isset($_POST['g-recaptcha-response']) ? $_POST['g-recaptcha-response'] : null;
 $erro_recaptcha = $GLOBALS;
-if(!is_null($captcha)){
-	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfpklIhAAAAAL2vuw8agNA4mkK_5--jkmHszDUY&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']));
-	if($res->success === true){
+if (!is_null($captcha)) {
+	$res = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfpklIhAAAAAL2vuw8agNA4mkK_5--jkmHszDUY&response=" . $captcha . "&remoteip=" . $_SERVER['REMOTE_ADDR']));
+	if ($res->success === true) {
 		//CAPTCHA validado!!!
 		header('location: index.php');
-	}
-	else{
-		return $erro_recaptcha['erro_recaptcha'] = 'reCAPTCHA inválido!';
+	} else {
+		$erro_recaptcha['erro_recaptcha'] = 'reCAPTCHA inválido!';
 		header('location: index.php');
 	}
 }
-?>
