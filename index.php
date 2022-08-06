95c24011ac34d766f107aeb2cc7fd09c228b27af
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-08-06 20:24:36 -0300

    script recaptcha

--- index.php
+++ index.php
@@ -118,7 +118,9 @@ if ($_POST) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src='https://www.google.com/recaptcha/api.js'></script>
+  <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+    async defer>
+</script>
 
   <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
