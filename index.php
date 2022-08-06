1a667af6f64f14a591b7248b72cca222dccfe3e2
nathan.maia <61885401+Nathan-maya@users.noreply.github.com>
2022-08-06 20:27:46 -0300

    implementar aviso de erro recaptcha

--- index.php
+++ index.php
@@ -118,9 +118,7 @@ if ($_POST) {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-    async defer>
-</script>
+  <script src='https://www.google.com/recaptcha/api.js'></script>
 
   <!-- google fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
